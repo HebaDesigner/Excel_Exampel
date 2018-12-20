d550db3f9f52874f6113883051ab7a849d98fea1
beboooooo <heba.designer@yahoo.com>
2018-12-20 02:56:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/beboooooo/excel_exampel/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.beboooooo.excel_exampel;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import java.io.InputStream;
+import java.util.Iterator;
+public class MainActivity extends AppCompatActivity {
+    String TAG ="main";
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        readExcelFileFromAssets();
+    }
+    public void readExcelFileFromAssets() {
+        try {
+            InputStream myInput;
+            // initialize asset manager
+            AssetManager assetManager = getAssets();
+            //  open excel sheet
+            myInput = assetManager.open("myexcelsheet.xls");
+            // Create a POI File System object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            // We now need something to iterate through the cells.
+            Iterator<Row> rowIter = mySheet.rowIterator();
+            int rowno =0;
+            textView.append("\n");
+            while (rowIter.hasNext()) {
+                Log.e(TAG, " row no "+ rowno );
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                if(rowno !=0) {
+                    Iterator<Cell> cellIter = myRow.cellIterator();
+                    int colno =0;
+                    String sno="", date="", det="";
+                    while (cellIter.hasNext()) {
+                        HSSFCell myCell = (HSSFCell) cellIter.next();
+                        if (colno==0){
+                            sno = myCell.toString();
+                        }else if (colno==1){
+                            date = myCell.toString();
+                        }else if (colno==2){
+                            det = myCell.toString();
+                        }
+                        colno++;
+                        Log.e(TAG, " Index :" + myCell.getColumnIndex() + " -- " + myCell.toString());
+                    }
+                    textView.append( sno + " -- "+ date+ "  -- "+ det+"\n");
+                }
+                rowno++;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "error "+ e.toString());
+        }
+    }
+}
